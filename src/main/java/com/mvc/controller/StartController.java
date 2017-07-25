837edbeee0fb977d4671d145e4a640f17699508b
zlotyy <arek.gajewski.123@gmail.com>
2017-07-25 14:02:21 +0200

    postawienie aplikacji, zaleznosci, struktura folderow

---
+++ src/main/java/com/mvc/controller/StartController.java
@@ -0,0 +1,16 @@
+package com.mvc.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class StartController {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/")
+    public String returnStartPage(){
+        return  "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zloty
+  Date: 2017-07-25
+  Time: 13:45
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+
+<body>
+TEST
+</body>
+</html>
