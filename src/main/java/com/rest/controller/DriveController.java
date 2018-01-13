114f514bca376969511599efaeba8cab074b576c
zlotyy <arek.gajewski.123@gmail.com>
2018-01-13 20:52:18 +0100

    RestApi - wyszukiwanie i rezerwacja przejazdu

--- src/main/java/com/rest/controller/DriveController.java
+++ src/main/java/com/rest/controller/DriveController.java
@@ -3,6 +3,7 @@ package com.rest.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mvc.dto.DriveDTO;
+import com.mvc.dto.SearchDrivesDTO;
 import com.mvc.helpers.DateFormatHelper;
 import com.mvc.helpers.ServiceResult;
 import com.mvc.model.BookingModel;
@@ -43,7 +44,9 @@ public class DriveController {
     IBookingService bookingService;
 
 
-
+    /**
+     * REST - DODAJ NOWY PRZEJAZD
+     */
     @RequestMapping(value = "/addNewDrive", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> addNewDrive(@RequestBody DriveDTO driveDTO, //@SessionAttribute("session_restTokensList") ArrayList<String> session_restTokens,
                                          @RequestHeader() HashMap<String, String> header){
@@ -121,6 +124,10 @@ public class DriveController {
 
 
 
+
+    /**
+     * REST - POBIERZ MOJE PRZEJAZDY
+     */
     @RequestMapping(value = "/myDrives", method = RequestMethod.GET)
     public ResponseEntity<?> getMyDrives(//@SessionAttribute("session_restTokensList") ArrayList<String> session_restTokens,
                                          @RequestHeader() HashMap<String, String> header){
@@ -160,6 +167,10 @@ public class DriveController {
 
 
 
+
+    /**
+     * REST - USUN PRZEJAZD
+     */
     @RequestMapping(value = "/myDrives/delete", method = RequestMethod.POST)
     public ResponseEntity<?> deleteDrive(@RequestBody HashMap<String, Long> driveIdMap){
 
@@ -192,6 +203,10 @@ public class DriveController {
 
 
 
+
+    /**
+     * REST - POBIERZ MOJE REZERWACJE
+     */
     @RequestMapping(value = "/myBookings", method = RequestMethod.GET)
     public ResponseEntity<?> getMyBookings(@RequestHeader() HashMap<String, String> header){
 
@@ -227,6 +242,10 @@ public class DriveController {
 
 
 
+
+    /**
+     * REST - ODWOLAJ REZERWACJE
+     */
     @RequestMapping(value = "/myBookings/unbookDrive", method = RequestMethod.POST)
     public ResponseEntity<?> unbookDrive(@RequestBody HashMap<String, Long> driveIdMap,
                                          @RequestHeader() HashMap<String, String> header){
@@ -262,6 +281,10 @@ public class DriveController {
 
 
 
+
+    /**
+     * REST - POBIERZ SZCZEGOLY PRZEJAZDU
+     */
     @RequestMapping(value = "/getDriveDetails", method = RequestMethod.POST)
     public ResponseEntity<?> getDriveDetails(@RequestBody HashMap<String, Long> driveIdMap,
                                          @RequestHeader HashMap<String, String> header){
@@ -306,6 +329,10 @@ public class DriveController {
 
 
 
+
+    /**
+     * REST - EDYTUJ PRZEJAZD
+     */
     @RequestMapping(value = "/editDrive", method = RequestMethod.POST)
     public ResponseEntity<?> editDrive(@RequestBody DriveDTO driveDTO,
                                          @RequestHeader() HashMap<String, String> header){
@@ -374,6 +401,119 @@ public class DriveController {
             log.info("REST - nie udalo sie edytowac przejazdu");
             return new ResponseEntity<>(new EmptyJsonResponse(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+
+
+    /**
+     * REST - WYSZUKAJ PRZEJAZDY
+     */
+    @RequestMapping(value = "/searchDrives", method = RequestMethod.POST)
+    public ResponseEntity<?> searchDrives(@RequestBody SearchDrivesDTO searchDrivesDTO,
+                                          @RequestHeader HashMap<String, String> header){
+
+        log.info("RESTOWA METODA - WYSZUKAJ PRZEJAZD");
+
+
+        ServiceResult<List<DriveModel>> result;
+        List<DriveModel> drives;
+        boolean isRoundTrip = false;
+        Calendar startDate = null;
+        Calendar returnDate = null;
+        String header_id = header.get("userid");
+        Long userId = Long.parseLong(header_id);
+        UserModel user = userService.getUser(userId).getData();
+
+        // Parsowanie daty na Calendar
+        DateFormatHelper dateFormatHelper = new DateFormatHelper(searchDrivesDTO.getStartDate(), "yyyy-MM-dd HH:mm");
+        if(searchDrivesDTO.getStartDate() != null){
+            startDate = dateFormatHelper.stringToCalendar_DateTimeFormat();
+        }
+        dateFormatHelper = new DateFormatHelper(searchDrivesDTO.getReturnDate(), "yyyy-MM-dd HH:mm");
+        if(searchDrivesDTO.getReturnDate() != null){
+            returnDate = dateFormatHelper.stringToCalendar_DateTimeFormat();
+        }
+
+        // Parsowanie Stringa na boolean
+        if(searchDrivesDTO.getIsRoundTrip() != null) {
+            if (searchDrivesDTO.getIsRoundTrip().equals("true") || searchDrivesDTO.getIsRoundTrip().equals("Tak"))
+                isRoundTrip = true;
+        }
+
+        try {
+
+            result = driveService.getDrives(
+                    searchDrivesDTO.getStartPlace(),
+                    searchDrivesDTO.getArrivalPlace(),
+                    startDate,
+                    returnDate,
+                    isRoundTrip,
+                    searchDrivesDTO.getMaxCost(),
+                    searchDrivesDTO.getLuggageSize(),
+                    user
+            );
+        } catch (Exception e){
+            log.info("REST - nie udalo sie wyszukac przejazdow");
+            return new ResponseEntity<>(new EmptyJsonResponse(), HttpStatus.BAD_REQUEST);
+        }
+
+
+        if(result.isValid()) {
+            drives = result.getData();
+            JSONObject drivesJSON;
+
+            // parsowanie listy przejazdow na JSONA
+            try {
+                drivesJSON = JsonHelper.getJSONObjectFromList(drives, "searchedDrives");
+            } catch (Exception e){
+                return new ResponseEntity<>(new EmptyJsonResponse(), HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+
+            return new ResponseEntity<>(drivesJSON.toString(), HttpStatus.OK);
+        } else {
+            log.info("REST - nie udalo sie wyszukac przejazdow");
+            return new ResponseEntity<>(new EmptyJsonResponse(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
     }
+
+
+
+
+    /**
+     * REST - ZAREZERWUJ
+     */
+    @RequestMapping(value = "/bookDrive", method = RequestMethod.POST)
+    public ResponseEntity<?> bookDrive(@RequestBody HashMap<String, Long> driveIdMap,
+                                         @RequestHeader() HashMap<String, String> header){
+
+        log.info("RESTOWA METODA - ZAREZERWUJ");
+
+
+        ServiceResult<BookingModel> result;
+
+        Long driveId = driveIdMap.get("driveId");
+        DriveModel drive = driveService.getDrive(driveId).getData();
+
+        String header_id = header.get("userid");
+        Long userId = Long.parseLong(header_id);
+        UserModel passenger = userService.getUser(userId).getData();
+
+        try {
+            result = bookingService.bookDrive(passenger, drive);
+        } catch (Exception e){
+            log.info("REST - nie udalo sie zarezerwowac przejazdu");
+            return new ResponseEntity<>(new EmptyJsonResponse(), HttpStatus.BAD_REQUEST);
+        }
+
+        if(result.isValid()) {
+            return new ResponseEntity<>(new EmptyJsonResponse(), HttpStatus.OK);
+        } else {
+            log.info("REST - nie udalo sie usunac przejazdu");
+            return new ResponseEntity<>(new EmptyJsonResponse(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+
 }
